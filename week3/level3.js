f213030c548e44a5e884b8c2903b9a6b7c9ef9e2
MKlinlin <3024672738@qq.com>
2023-05-11 22:36:13 +0800

    week3

--- week3/level3.js
+++ week3/level3.js
@@ -1,4 +1,4 @@
-let person={weight:80};
-let human=person
+let person={weight:80};//定义一个对象
+let human=person//定义一个新的对象human赋值为person
 console.log(person)
-console.log(human)
+console.log(human)//输出human，和输出person结果一致，实现深拷贝
